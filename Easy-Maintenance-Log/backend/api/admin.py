f54cb9630b1f20fe7ddc6ef645a6ac8b2a04e1ee
Emmett Grebe <emmettgrebe@gmail.com>
2026-09-13 18:36:10 -0400

    Django admin.

--- Easy-Maintenance-Log/backend/api/admin.py
+++ Easy-Maintenance-Log/backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Car, Service
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'descriptor', 'milage', 'color')
+    search_fields = ('make', 'model', 'descriptor')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('car', 'title', 'milage', 'date', 'next_milage', 'next_time', 'milage_dependant', 'time_dependant')
+    search_fields = ('title', 'description')
+    list_filter = ('milage_dependant', 'time_dependant')
