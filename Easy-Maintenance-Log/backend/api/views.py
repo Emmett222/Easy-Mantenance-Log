e7f7677217fc4c6cac55eb1bfdea48367dc0e5ec
Emmett Grebe <emmettgrebe@gmail.com>
2026-09-13 18:41:47 -0400

    Django viewsets.

--- Easy-Maintenance-Log/backend/api/views.py
+++ Easy-Maintenance-Log/backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Car, Service
+from .serializers import CarSerializer, ServiceSerializer
 
-# Create your views here.
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
