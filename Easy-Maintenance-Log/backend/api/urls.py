109d83817c612e6cc8ffc028dab20e9c39d2bb94
Emmett Grebe <emmettgrebe@gmail.com>
2026-09-13 18:46:37 -0400

    Django API URLs

---
+++ Easy-Maintenance-Log/backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CarViewSet, ServiceViewSet
+
+router = DefaultRouter()
+router.register(r'cars', CarViewSet, basename='car')
+router.register(r'services', CarViewSet, basename='service')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
